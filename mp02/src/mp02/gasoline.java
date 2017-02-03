0682c02dd26cc1c65de86beb36be8e409f31530a
ernestpascual <eepascual@gmail.com>
2017-02-03 14:23:26 +0800

    Add files via upload

--- mp02/src/mp02/gasoline.java
+++ mp02/src/mp02/gasoline.java
@@ -1,8 +1,11 @@
 package mp02;
 
 import java.io.IOException;
-
+import java.util.Date;
 import utility.Reader;
+import java.text.DecimalFormat;
+import java.text.*;
+
 
 public class gasoline {
 
@@ -26,12 +29,21 @@ public class gasoline {
 	//format heading of the receipt
 	// TO BE EDITED
 	public static void receiptHeader(){
-		System.out.println("\t\t\tGasoline Station");
+		 Date dNow = new Date( );
+	      SimpleDateFormat ft = 
+	      new SimpleDateFormat ("yyyy.MM.dd '\n\t\t\tat' hh:mm:ss");
+		System.out.println("\n\n\n\n\t\t\tEEP Gasoline Station" 
+							+ "\n\t\t\tTaft Ave., Malate" 
+							+ "\n\t\t\tMetro Manila"
+							+"\n\t\t\tPhilippines"
+							+"\n\n\t\t\tDate: " + ft.format(dNow)
+							+"\n\t\t\tReceipt No. 983923\n");
 	}
 	
 	/* validates if the input string is within the choices
 	 else it will output error message and loop again
 	 */
+	//bug reverts to prompter if executed
 	public static void gasVerify(String gasType)
 			throws IOException{
 		switch(gasType){
@@ -44,16 +56,20 @@ public class gasoline {
 		default:
 			System.out.println("\tGas type not found! ");
 			prompter();
+			break;
+		
 		}
-	}
+		
+		}
+	
 	
 	// method in adding the vat
 	public static void addVat(int price){
 		// two decimal points
 		double vat = price * 0.12;
-		System.out.println("\tVAT: Php " + vat);
+		System.out.println("\t\t\tVAT: Php " + (new DecimalFormat("##.##").format(vat)));
 		double total = price + vat;
-		System.out.println("\tTOTAL AMOUNT: Php" + total);
+		System.out.println("\t\t\tTOTAL AMOUNT: Php " + total);
 		}
 
 	//computing the total amount to be paid with VAT
@@ -63,21 +79,21 @@ public class gasoline {
 	switch(gasType){
 	//premium
 	case "premium": 
-		System.out.println("\tFuel Type: Premium");
+		System.out.println("\t\t\tFuel Type: Premium");
 		total = liters * 50;
 		break;
 	//diesel
 	case "diesel": 
-		System.out.println("\tFuel Type: Diesel");
+		System.out.println("\t\t\tFuel Type: Diesel");
 		total = liters * 38;
 		break;
 	//unleaded
 	case "unleaded": 
-		System.out.println("\tFuel Type: Unleaded");
+		System.out.println("\t\t\tFuel Type: Unleaded");
 		total = liters * 44;
 		break;
 	}
-	System.out.println("\tPurchase amount: Php " + total);
+	System.out.println("\t\t\tPurchase amount: Php " + total);
 	addVat(total);
 	continueApp();
 	}
@@ -86,23 +102,27 @@ public class gasoline {
 	public static void continueApp()
 		throws IOException{
 		String cont = Reader.readString
-				("\tContinue? Y/N: ").toUpperCase();
+				("\n\t\t\tContinue? Y/N: ").toUpperCase();
 		if(cont.equals("Y")){
 			prompter();
-		} else if (cont.equals("N")){
-			System.out.println("Program Terminated");
 		} else {
-			Reader.readString
-			("\tContinue? Y/N: ");
+			System.out.println("Press any key to exit.");
+		}
 		}
 
 	}
 
 	
-	}
+	
 	
 // format
-// Note: Fix decimal points (2 decimal points)
+// fix bug at prompter
+/*
+ * NOTE - The gasoline type must be correct (either unleaded, diesel or premium) 
+ * and number of liters ordered must not be negative or zero. If one is invalid or 
+ * both of them are invalid, any computations MUST NOT be performed. You have to repeat its 
+ * data entry until the data entered is correct. 
+ */
 
 
 
